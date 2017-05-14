0675f04719e3d1dd1d1e62cde9b35ef7d94b9749
doushan12 <dous12@126.com>
2017-05-14 17:08:24 +0800

    选择特征

--- GrowTree.m
+++ GrowTree.m
@@ -3,7 +3,7 @@ function []=GrowTree(train,label,thredhold)
 Index=(doclabel>0);
 if_choosed=true(size(train,2),1);
 imp=count_impurity(label,Index);
-feature=choose_feature(train,label,imp,if_choosed);
+feature=choose_feature(train,label,imp,if_choosed,Index);
 if_choosed(feature)=false;
 
 
