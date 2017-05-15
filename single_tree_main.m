17701d7e68f8b88be7d728ddb4c18448758cd439
doushan12 <dous12@126.com>
2017-05-15 23:56:00 +0800

    报告

--- single_tree_main.m
+++ single_tree_main.m
@@ -1,11 +1,11 @@
-%单一决策树
-%% 初始化
+%??????????
+%% ??????
 clear;
 load('Sogou_webpage.mat')
 doclabel = int16(doclabel);
 wordMat = int16(wordMat);
 
-%% 取wordmat的1/5为测试集，4/5为训练集
+%% ??wordmat??1/5??????????4/5????????
 delta=5;
 n=length(doclabel);
 k=randperm(n);
@@ -16,8 +16,8 @@ train_label=doclabel_rand(1:0.8*n,:);
 test_word=wordMat_rand(0.8*n+1:end,:);
 test_label=doclabel_rand(0.8*n+1:end,:);
 
-%% 决策树生成
-threshold=[0.1];
+%% ??????????
+threshold=[0.07];
 train_acc=zeros(length(threshold),1);
 test_acc=zeros(length(threshold),1);
 for i=1:length(threshold)
